01689b398bad1c22c8dc56f2d0846c16b52b0e2d
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-01-31 15:23:15 -0500

    practce

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.post('/users', (req, res) => {
+    const {name, age, email} = req.body;
+
+
+    const user = { name, age, email};
+
+    const result = `My name is ${user.name}, I am ${user.age} years old and my email is ${user.email};`
+
+    res.send(result);
+})
+
+app.listen(3000, () => {
+    console.log('We be listening on 3000');
+})
